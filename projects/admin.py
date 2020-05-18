65caaae227b283c92337587cf3edfff9e415ea0d
rustamrv <rystamrv@gmail.com>
2020-05-18 09:44:14 +0300

    Added projects app.

---
+++ projects/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Category, Subcategory, Project
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    list_filter = ('name', )
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', )
+    list_filter = ('name', 'category', )
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', )
+    list_filter = ('name', 'category', 'subcategory', )
+    prepopulated_fields = {"slug": ("name",)}
---
+++ projects/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "subcategories"
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, null=True, blank=True,
+                                 on_delete=models.CASCADE)
+    subcategory = models.ManyToManyField(Subcategory)
+
+    class Meta:
+        verbose_name_plural = "projects"
+
+    def __str__(self):
+        return self.name
