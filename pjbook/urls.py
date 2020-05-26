ba6648e88c7ca73136086d828d505795c330821b
rustamrv <rystamrv@gmail.com>
2020-05-26 20:30:56 +0300

    pull pjbook

--- pjbook/urls.py
+++ pjbook/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .view import BaseView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
-    path('', BaseView.as_view(), name='home'),
     path('account/', include('accounts.urls'))
 ]
