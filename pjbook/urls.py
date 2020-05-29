6e2f7f42b15f74e9d1bd88b3d9cddcd625ae0835
rustamrv <rystamrv@gmail.com>
2020-05-29 20:39:07 +0300

    fixed

--- pjbook/urls.py
+++ pjbook/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chat.urls')), 
+    path('chat/', include('chat.urls')),
     path('account/', include('accounts.urls'))
 ]
