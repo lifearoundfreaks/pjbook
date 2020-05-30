7d40d10ac002ac05c3c8ad3fa75cfa936c579657
rustamrv <rystamrv@gmail.com>
2020-05-30 14:22:46 +0300

    added projects application

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import DetailCategory, ProjectView
 
 
 urlpatterns = [
-    path('projects/<slug>', DetailCategory.as_view(), name='detail_category')
+    ...
 ]
