f7e24ae96be9769aff82905e153e7c8182c6accf
rustamrv <rystamrv@gmail.com>
2020-05-30 14:05:24 +0300

    uodate

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,5 @@ from .views import DetailCategory, ProjectView
 
 
 urlpatterns = [
-    path('', ProjectView.as_view(), name='home'),
     path('projects/<slug>', DetailCategory.as_view(), name='detail_category')
 ]
