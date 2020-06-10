b8b16d41940c479986ce9635c738163dc2de57c0
ZefRi19 <vitaliy.kucherenko1219@gmail.com>
2020-06-10 21:27:40 +0300

    Фикс регистрации

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import AccessMixin, LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse
 from django.views.generic import FormView
 from accounts.forms import CreateUserForm
@@ -6,12 +8,29 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 
 
-class RegistrationView(FormView):
+class SignInView(AccessMixin, LoginView):
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+
+class SignOutView(LoginRequiredMixin, LogoutView):
+    next_page = 'popular_projects'
+
+
+class RegistrationView(AccessMixin, FormView):
     form_class = CreateUserForm
     template_name = "accounts/registration.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
     def get_success_url(self):
-        return reverse('home')
+        return reverse('popular_projects')
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
